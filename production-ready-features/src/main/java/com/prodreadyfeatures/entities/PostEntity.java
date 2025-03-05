662738149dca389dba7610320b66186b97cfbf18
sayan4545 <sayanchatterjee3010@gmail.com>
2025-03-06 00:43:26 +0530

    While post request, recieved null , so added getters and setters by adding @Getter and @Setter annotation in PostEntity class

--- production-ready-features/src/main/java/com/prodreadyfeatures/entities/PostEntity.java
+++ production-ready-features/src/main/java/com/prodreadyfeatures/entities/PostEntity.java
@@ -2,12 +2,16 @@ package com.prodreadyfeatures.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "posts")
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class PostEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
